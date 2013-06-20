1225b423840417d7d22806a4d4efb7ca04333fce
Aaron Weiss <aaronweiss74@gmail.com>
2013-06-19 21:30:02 -0400

    Added solution for problem 12. I should probably make it faster.

---
+++ euler12.hs
@@ -0,0 +1,14 @@
+divisors :: Int -> [Int]
+divisors n = 1 : filter ((== 0) . rem n) [2..n]
+
+countDivisors :: Int -> Int
+countDivisors n = length $ divisors n
+
+triangle :: Int -> Int
+triangle n = sum [1..n]
+	
+euler12 :: Int -> Int
+euler12 n = head $ filter divisorCheck $ map triangle [1..]
+	where divisorCheck x = (countDivisors x) > n
+
+main = print $ euler12 500
