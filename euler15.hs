5dabcb99a5609eec562038c5472d0bef27634855
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-22 21:19:24 -0400

    Made the factorial function used in euler15 clearer.

--- euler15.hs
+++ euler15.hs
@@ -1,6 +1,5 @@
 factorial :: (Integral a) => a -> a
-factorial 0 = 1
-factorial n = n * (factorial $ n - 1)
+factorial = foldr (*) 1 . enumFromTo 1
 
 lattice :: (Integral a) => a -> a
 lattice n = (factorial $ 2 * n) `div` (factorial n)^2
