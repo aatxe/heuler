550b36bea2261fd1f6b2f21ad2fafbc5b5d15f2e
Aaron Weiss <aaronweiss74@gmail.com>
2013-08-11 18:31:19 -0400

    Improved euler25 solution.

--- euler25.hs
+++ euler25.hs
@@ -1,9 +1,6 @@
-import Data.List  (findIndex)
-import Data.Maybe (fromJust)
-
 euler25 :: Int -> Int
-euler25 n = fromJust $ findIndex (\x -> (>) x $ (^) 10 $ n - 1) fibs
-  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+euler25 n = length $ takeWhile (\x -> (<) x $ (^) 10 $ n - 1) fibs
+  where fibs = scanl (+) 0 (1 : fibs)
 
 main :: IO ()
 main = print $ euler25 1000
